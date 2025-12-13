7137548ce14ae8dee2071da33822485c9b5a4a78
Divyanshu Pairhar <divyanshu1447@gmail.com>
2025-12-13 18:02:22 +0530

    init tests

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+mod collection;
 fn main() {
     println!("Hello, world!");
+}
+
+#[cfg(test)]
+mod test {
+    mod vector {
+        use std::io::error;
+
+        use crate::collection::base::collectable;
+        use crate::collection::vector::vector;
+        #[test]
+        fn vector_init() -> result<(), error> {
+            let vector: vector<string> = vector::new();
+
+            assert_eq!(vector.get_len(), 0);
+            return ok(());
+        }
+    }
 }
